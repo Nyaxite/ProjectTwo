232ddcf80aa49e59c3809de41163e25de64119ac
Evan Pugh <ev44o@outlook.com>
2012-12-09 22:16:32 -0500

    Added Splash + minor changes
    Added splash screen with slide in and out effect.

--- Monu-MentalMath/gen/ca/michael/evan/monu_mentalmath/R.java
+++ Monu-MentalMath/gen/ca/michael/evan/monu_mentalmath/R.java
@@ -8,29 +8,33 @@
 package ca.michael.evan.monu_mentalmath;
 
 public final class R {
+    public static final class anim {
+        public static final int slidein=0x7f040000;
+        public static final int slideout=0x7f040001;
+    }
     public static final class attr {
     }
     public static final class color {
-        public static final int black=0x7f040000;
-        public static final int blue=0x7f040002;
-        public static final int green=0x7f040005;
-        public static final int red=0x7f040003;
-        public static final int white=0x7f040001;
-        public static final int yellow=0x7f040004;
+        public static final int black=0x7f050000;
+        public static final int blue=0x7f050002;
+        public static final int green=0x7f050005;
+        public static final int red=0x7f050003;
+        public static final int white=0x7f050001;
+        public static final int yellow=0x7f050004;
     }
     public static final class dimen {
-        public static final int dimen_10dp=0x7f050007;
-        public static final int dimen_20dp=0x7f05000b;
-        public static final int dimen_50dp=0x7f050008;
-        public static final int dimen_5dp=0x7f05000a;
-        public static final int font_10=0x7f050004;
-        public static final int font_12=0x7f050006;
-        public static final int font_15=0x7f050005;
-        public static final int font_20=0x7f050002;
-        public static final int font_25=0x7f050009;
-        public static final int font_30=0x7f050000;
-        public static final int font_40=0x7f050003;
-        public static final int font_50=0x7f050001;
+        public static final int dimen_10dp=0x7f060007;
+        public static final int dimen_20dp=0x7f06000b;
+        public static final int dimen_50dp=0x7f060008;
+        public static final int dimen_5dp=0x7f06000a;
+        public static final int font_10=0x7f060004;
+        public static final int font_12=0x7f060006;
+        public static final int font_15=0x7f060005;
+        public static final int font_20=0x7f060002;
+        public static final int font_25=0x7f060009;
+        public static final int font_30=0x7f060000;
+        public static final int font_40=0x7f060003;
+        public static final int font_50=0x7f060001;
     }
     public static final class drawable {
         public static final int background=0x7f020000;
@@ -42,104 +46,108 @@ public final class R {
         public static final int green_answer_button=0x7f020006;
         public static final int green_button=0x7f020007;
         public static final int ic_launcher=0x7f020008;
-        public static final int red_answer_button=0x7f020009;
-        public static final int red_button=0x7f02000a;
-        public static final int wood_tile_light=0x7f02000b;
-        public static final int yellow_answer_button=0x7f02000c;
-        public static final int yellow_button=0x7f02000d;
+        public static final int monu_mental_math_splashscreen=0x7f020009;
+        public static final int red_answer_button=0x7f02000a;
+        public static final int red_button=0x7f02000b;
+        public static final int wood_tile_light=0x7f02000c;
+        public static final int yellow_answer_button=0x7f02000d;
+        public static final int yellow_button=0x7f02000e;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f0a0000;
-        public static final int answerButton=0x7f0a001d;
-        public static final int answerButton1=0x7f0a0010;
-        public static final int answerButton2=0x7f0a0011;
-        public static final int answerButton3=0x7f0a0013;
-        public static final int answerButton4=0x7f0a0014;
-        public static final int answerEditText=0x7f0a001b;
-        public static final int buttonFour=0x7f0a000a;
-        public static final int buttonOne=0x7f0a0006;
-        public static final int buttonThree=0x7f0a0009;
-        public static final int buttonTwo=0x7f0a0007;
-        public static final int clearButton=0x7f0a002a;
-        public static final int eightButton=0x7f0a0026;
-        public static final int fiveButton=0x7f0a0022;
-        public static final int fleetingFiguresBotton=0x7f0a0016;
-        public static final int fourButton=0x7f0a0021;
-        public static final int infoTextView=0x7f0a0003;
-        public static final int makeNegativeCheckBox=0x7f0a001c;
-        public static final int mentalMathButton=0x7f0a0015;
-        public static final int menu_settings=0x7f0a002b;
-        public static final int nineButton=0x7f0a0027;
-        public static final int numberOneTextView=0x7f0a0018;
-        public static final int numberTwoTextView=0x7f0a001a;
-        public static final int oneButton=0x7f0a001e;
-        public static final int operatorTextView=0x7f0a0019;
-        public static final int questionNumberTextView=0x7f0a000e;
-        public static final int questionTextView=0x7f0a000c;
-        public static final int scoreTextView=0x7f0a0004;
-        public static final int sevenButton=0x7f0a0025;
-        public static final int sixButton=0x7f0a0023;
-        public static final int tableRow0=0x7f0a0017;
-        public static final int tableRow1=0x7f0a0001;
-        public static final int tableRow2=0x7f0a0005;
-        public static final int tableRow3=0x7f0a0008;
-        public static final int tableRow4=0x7f0a000b;
-        public static final int tableRow5=0x7f0a000f;
-        public static final int tableRow6=0x7f0a0012;
-        public static final int tableRow7=0x7f0a000d;
-        public static final int tableRow8=0x7f0a0024;
-        public static final int tableRow9=0x7f0a0028;
-        public static final int threeButton=0x7f0a0020;
-        public static final int timerTextView=0x7f0a0002;
-        public static final int twoButton=0x7f0a001f;
-        public static final int zeroButton=0x7f0a0029;
+        public static final int LinearLayout1=0x7f0b0001;
+        public static final int answerButton=0x7f0b001e;
+        public static final int answerButton1=0x7f0b0011;
+        public static final int answerButton2=0x7f0b0012;
+        public static final int answerButton3=0x7f0b0014;
+        public static final int answerButton4=0x7f0b0015;
+        public static final int answerEditText=0x7f0b001c;
+        public static final int buttonFour=0x7f0b000b;
+        public static final int buttonOne=0x7f0b0007;
+        public static final int buttonThree=0x7f0b000a;
+        public static final int buttonTwo=0x7f0b0008;
+        public static final int clearButton=0x7f0b002b;
+        public static final int eightButton=0x7f0b0027;
+        public static final int fiveButton=0x7f0b0023;
+        public static final int fleetingFiguresBotton=0x7f0b0017;
+        public static final int fourButton=0x7f0b0022;
+        public static final int imageView1=0x7f0b0000;
+        public static final int infoTextView=0x7f0b0004;
+        public static final int makeNegativeCheckBox=0x7f0b001d;
+        public static final int mentalMathButton=0x7f0b0016;
+        public static final int menu_settings=0x7f0b002c;
+        public static final int nineButton=0x7f0b0028;
+        public static final int numberOneTextView=0x7f0b0019;
+        public static final int numberTwoTextView=0x7f0b001b;
+        public static final int oneButton=0x7f0b001f;
+        public static final int operatorTextView=0x7f0b001a;
+        public static final int questionNumberTextView=0x7f0b000f;
+        public static final int questionTextView=0x7f0b000d;
+        public static final int scoreTextView=0x7f0b0005;
+        public static final int sevenButton=0x7f0b0026;
+        public static final int sixButton=0x7f0b0024;
+        public static final int tableRow0=0x7f0b0018;
+        public static final int tableRow1=0x7f0b0002;
+        public static final int tableRow2=0x7f0b0006;
+        public static final int tableRow3=0x7f0b0009;
+        public static final int tableRow4=0x7f0b000c;
+        public static final int tableRow5=0x7f0b0010;
+        public static final int tableRow6=0x7f0b0013;
+        public static final int tableRow7=0x7f0b000e;
+        public static final int tableRow8=0x7f0b0025;
+        public static final int tableRow9=0x7f0b0029;
+        public static final int threeButton=0x7f0b0021;
+        public static final int timerTextView=0x7f0b0003;
+        public static final int twoButton=0x7f0b0020;
+        public static final int zeroButton=0x7f0b002a;
     }
     public static final class integer {
-        public static final int five=0x7f060000;
+        public static final int five=0x7f070000;
     }
     public static final class layout {
-        public static final int fleeting_figures=0x7f030000;
-        public static final int main_menu=0x7f030001;
-        public static final int mental_math=0x7f030002;
+        public static final int activity_splash_screen=0x7f030000;
+        public static final int fleeting_figures=0x7f030001;
+        public static final int main_menu=0x7f030002;
+        public static final int mental_math=0x7f030003;
     }
     public static final class menu {
-        public static final int fleeting_figures=0x7f090000;
-        public static final int main_menu=0x7f090001;
-        public static final int mental_math=0x7f090002;
+        public static final int activity_splash_screen=0x7f0a0000;
+        public static final int fleeting_figures=0x7f0a0001;
+        public static final int main_menu=0x7f0a0002;
+        public static final int mental_math=0x7f0a0003;
     }
     public static final class string {
-        public static final int a_line=0x7f070006;
-        public static final int app_name=0x7f070000;
-        public static final int blue=0x7f07001d;
-        public static final int c=0x7f070008;
-        public static final int calculate=0x7f070019;
-        public static final int clear=0x7f070009;
-        public static final int done=0x7f070015;
-        public static final int fleeting_figures=0x7f07001b;
-        public static final int green=0x7f07001e;
-        public static final int hello_world=0x7f070001;
-        public static final int make_negative=0x7f070007;
-        public static final int mental_math=0x7f070003;
-        public static final int menu_settings=0x7f070002;
-        public static final int nothing=0x7f070016;
-        public static final int number_eight=0x7f070010;
-        public static final int number_five=0x7f07000e;
-        public static final int number_four=0x7f07000d;
-        public static final int number_nine=0x7f070012;
-        public static final int number_one=0x7f07000a;
-        public static final int number_seven=0x7f070011;
-        public static final int number_six=0x7f07000f;
-        public static final int number_three=0x7f07000c;
-        public static final int number_two=0x7f07000b;
-        public static final int number_zero=0x7f070013;
-        public static final int operator_addition=0x7f070014;
-        public static final int red=0x7f07001c;
-        public static final int score_zero=0x7f070017;
-        public static final int title_activity_fleeting_figures=0x7f07001a;
-        public static final int title_activity_mental_math=0x7f070004;
-        public static final int yellow=0x7f07001f;
-        public static final int zero_seconds=0x7f070018;
-        public static final int zero_seconds_zero_milliseconds=0x7f070005;
+        public static final int a_line=0x7f080006;
+        public static final int app_name=0x7f080000;
+        public static final int blue=0x7f08001d;
+        public static final int c=0x7f080008;
+        public static final int calculate=0x7f080019;
+        public static final int clear=0x7f080009;
+        public static final int done=0x7f080015;
+        public static final int fleeting_figures=0x7f08001b;
+        public static final int green=0x7f08001e;
+        public static final int hello_world=0x7f080001;
+        public static final int make_negative=0x7f080007;
+        public static final int mental_math=0x7f080003;
+        public static final int menu_settings=0x7f080002;
+        public static final int nothing=0x7f080016;
+        public static final int number_eight=0x7f080010;
+        public static final int number_five=0x7f08000e;
+        public static final int number_four=0x7f08000d;
+        public static final int number_nine=0x7f080012;
+        public static final int number_one=0x7f08000a;
+        public static final int number_seven=0x7f080011;
+        public static final int number_six=0x7f08000f;
+        public static final int number_three=0x7f08000c;
+        public static final int number_two=0x7f08000b;
+        public static final int number_zero=0x7f080013;
+        public static final int operator_addition=0x7f080014;
+        public static final int red=0x7f08001c;
+        public static final int score_zero=0x7f080017;
+        public static final int title_activity_fleeting_figures=0x7f08001a;
+        public static final int title_activity_mental_math=0x7f080004;
+        public static final int yellow=0x7f08001f;
+        public static final int zero_seconds=0x7f080018;
+        public static final int zero_seconds_zero_milliseconds=0x7f080005;
     }
     public static final class style {
         /** 
@@ -152,10 +160,10 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f080000;
+        public static final int AppBaseTheme=0x7f090000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080001;
+        public static final int AppTheme=0x7f090001;
     }
 }
---
+++ Monu-MentalMath/src/ca/michael/evan/monu_mentalmath/SplashScreen.java
@@ -0,0 +1,41 @@
+/*
+*	Name: Mental Math (ca.michael.evan.monu_mentalmath / MentalMath.java)
+*	Authors: Michael Burnie, Evan Pugh
+*	Date (due): 2012/12/14
+*	Description: The splash screen is shown for 4 seconds using a custom made 
+*				 image and a slide in and out effect.
+*/
+package ca.michael.evan.monu_mentalmath;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.content.Intent;
+
+public class SplashScreen extends Activity
+{
+	private static final int SPLASHSCREEN_DISP_TIME = 4000;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash_screen);
+		
+		new Handler().postDelayed(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				Intent mainActivityIntent = new Intent(SplashScreen.this, MainMenu.class);
+				SplashScreen.this.startActivity(mainActivityIntent);
+				
+				// end the splash screen activity so it can't be seen again
+				SplashScreen.this.finish();
+				
+				// slide the main menu in, and fade the splash screen out
+				overridePendingTransition(R.anim.slidein, R.anim.slideout);
+			}
+		}, SPLASHSCREEN_DISP_TIME);
+	}
+}
